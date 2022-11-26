c1f82850f0ed93c97b718547c1c32e4221d46c84
Shin0bik <118849550+Shin0bik@users.noreply.github.com>
2022-11-26 15:33:42 +0300

    Add files via upload

---
+++ Fabric.java
@@ -0,0 +1,20 @@
+public class Fabric {
+    public static void main(String[] Args) {
+        int furnace = 160;
+        int bigFurnace = 500;
+        boolean busyFurnace = false;
+        boolean busyBigFurnace = false;
+        int plan;
+        int remains;
+        int smelt;
+        if (plan > 160 && plan < 500 && busyFurnace); {
+            plan / furnace = smelt;
+        } else smelt = 0;
+        if (plan > 500 && busyBigFurnace); {
+            plan / bigFurnace = smelt;
+        } else smelt = 0;
+        if (plan > furnace + bigFurnace && busyBigFurnace && busyFurnace); {
+            plan / (furnace + bigFurnace) = smelt;
+        }
+    }
+}
