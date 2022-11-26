df4dfd31f273ef291911032b3f28da82471a7d9f
Shin0bik <118849550+Shin0bik@users.noreply.github.com>
2022-11-26 16:45:22 +0300

    Update Fabric.java

--- Fabric.java
+++ Fabric.java
@@ -7,14 +7,17 @@ public class Fabric {
         int plan;
         int remains;
         int smelt;
-        if (plan > 160 && plan < 500 && busyFurnace); {
-            plan / furnace = smelt;
-        } else smelt = 0;
-        if (plan > 500 && busyBigFurnace); {
-            plan / bigFurnace = smelt;
-        } else smelt = 0;
-        if (plan > furnace + bigFurnace && busyBigFurnace && busyFurnace); {
-            plan / (furnace + bigFurnace) = smelt;
+        if (plan > 160 && plan < 500 && busyFurnace) {
+            smelt = plan / furnace;
         }
+        else smelt = 0;
+        if (plan > 500 && busyBigFurnace) {
+            smelt = plan / bigFurnace;
+        }
+        else smelt = 0;
+        if ((plan > furnace + bigFurnace) && busyBigFurnace && busyFurnace) {
+            smelt = plan / (furnace + bigFurnace);
+        }
+
     }
 }
